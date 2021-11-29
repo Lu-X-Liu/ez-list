e79e5e10444704f7590de7baf0d1222bd953678e
Lu-X-Liu <91901553+Lu-X-Liu@users.noreply.github.com>
2021-11-28 21:04:44 -0600

    lists with categories first commit
    Basic features for lists with categories done

--- script.js
+++ script.js
@@ -46,48 +46,109 @@ const newListInput = document.querySelector('[data-new-list-input]');
 const newListInput2 = document.querySelector('[data-new-list-input-2]');
 const LOCAL_STORAGE_LIST_KEY = 'task.list';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
+const LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY = 'task.selectedCategoryId';
 const deletListBtns = document.querySelectorAll('[data-delete-list-button]');
+
+//categories
+const categoriesContainer = document.querySelector('[data-categories-container]');
+const categoryTemplate = document.getElementById('category-template');
+const newItemFormTemplate = document.getElementById('new-item-form-template');
+const createNewCategoryBtns = document.querySelectorAll('[data-create-new-category-btn]');
+const newCategoryFormPopup = document.querySelector('[data-new-category-form-popup]');
+const newCategoryForm = document.querySelector('[data-new-category-form]');
+const newCategoryInput = document.querySelector('[data-new-category-input]');
+const closeCategoryForm = document.querySelector('[data-close-category-form]');
+const deleteCategoryBtns = document.querySelectorAll('[data-delete-category-btn]');
+
 //items
 const listDisplayContainer = document.querySelector('[data-list-display-container]');
 const listTitleElement = document.querySelector('[data-list-title]');
 const listCountElement = document.querySelector('[data-list-count]');
-const tasksContainer = document.querySelector('[data-tasks]');
 const taskTemplate = document.getElementById('task-template');
-const newTaskForm = document.querySelector('[data-new-task-form]');
-const newTaskInput = document.querySelector('[data-new-task-input]');
 const clearCompletedTasksBtns = document.querySelectorAll('[data-clear-completed-tasks-btn]');
 
 
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+let selectedCategoryId = localStorage.getItem(LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY);
 
 // getting the selectedListId from target li
 listsContainer.addEventListener('click', e => {           
         if(e.target.tagName.toLowerCase() === 'li') {
             selectedListId = e.target.dataset.listId;
-            saveAndRender();
+            saveAndRenderAll();
         }
 });
 
 listsContainer2.addEventListener('click', e => {           
     if(e.target.tagName.toLowerCase() === 'li') {
         selectedListId = e.target.dataset.listId;
-        saveAndRender();
+        saveAndRenderAll();
         e.currentTarget.parentElement.parentElement.style.display = 'none';
     }
 });
 
-//(the 'input' below is the checkbox ) find selected task
-tasksContainer.addEventListener('click', e => {
-    if (e.target.tagName.toLowerCase() === 'input') {
+// select category or items in the category
+categoriesContainer.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'input' && e.target.type.toLowerCase() === 'radio'){
         const selectedList = lists.find(list => list.id === selectedListId);
-        const selectedTask = selectedList.tasks.find(task => task.id === e.target.id);
-        selectedTask.complete = e.target.checked; // = true
-        save();
-        renderTaskCount(selectedList);
+        const selectedCategory = selectedList.categories.find(category => category.id === e.target.id);
+        selectedCategory.selected = true;
+        selectedList.categories.forEach(
+            category => {
+                if (category !== selectedCategory) {
+                    category.selected = false;
+                }
+            }
+        )
+        selectedCategoryId = selectedCategory.id 
+        saveAndRenderselectedList();                                           
     }
-})
+});
 
+categoriesContainer.addEventListener('submit', e => {
+    e.preventDefault();
+    if (e.target.classList.contains('new-task-form')) {
+        const selectedList = lists.find(list => list.id === selectedListId);
+        const selectedCategory = selectedList.categories.find(category => category.id === selectedCategoryId);
+        const selectedCategoryWrapper = document.querySelector('.selected-category');
+        const selectedCategoryTaskContainer = selectedCategoryWrapper.querySelector('ul.items');        
+        const newTaskInput = selectedCategoryWrapper.querySelector('[data-new-task-input]');
+        
+        const taskName = newTaskInput.value;
+        if (taskName == null || taskName == '') return;
+        const categoryName = selectedCategoryTaskContainer.id;
+        const task = createTask(taskName,categoryName);
+        newTaskInput.value = null;
+        selectedCategory.tasks.push(task);
+        //saveAndRenderselectedList();
+        const tasksContainer = document.querySelectorAll('.category .items');
+        renderTaskCount(selectedList);
+        tasksContainer.forEach(taskContainer=> clearElement(taskContainer));
+        renderTasks(selectedList); 
+    }
+}); 
+
+//(the 'input' below is the checkbox ) find selected task
+categoriesContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'input' && e.target.type.toLowerCase() === 'checkbox') {
+        const selectedList = lists.find(list => list.id === selectedListId);
+        const categoryArr = selectedList.categories;
+        for(let i=0; i<categoryArr.length; i++){
+            const taskArr = categoryArr[i].tasks;
+            for(let j=0; j<taskArr.length; j++){
+                if (taskArr[j].id === e.target.id){
+                    const selectedTask = taskArr[j];
+                    selectedTask.complete = e.target.checked;
+                    save();  
+                    renderTaskCount(selectedList);       
+                }                    
+            }
+        }
+    }
+});
+
+//clear completed tasks
 clearCompletedTasksBtns.forEach(btn => {
     btn.addEventListener('click', clearCompleted);
     btn.addEventListener('click', hideParent);
@@ -95,24 +156,61 @@ clearCompletedTasksBtns.forEach(btn => {
 
 function clearCompleted() {
     const selectedList = lists.find(list => list.id === selectedListId);
-    selectedList.tasks = selectedList.tasks.filter(task => !task.complete);
-    saveAndRender();
+    selectedList.categories.forEach(category => {
+        category.tasks = category.tasks.filter(task => !task.complete);              
+    })
+    saveAndRenderAll();
 }
-
+//delete list
 deletListBtns.forEach( btn => {
-    btn.addEventListener('click', deletList);
+    btn.addEventListener('click', deleteList);
     btn.addEventListener('click', hideParent);
 });
 
-function deletList() {
+function deleteList() {
     lists = lists.filter(list => list.id !== selectedListId);
     selectedListId = "null";
-    saveAndRender();
+    saveAndRenderAll();
 }
+
 
 function hideParent(e) {
     e.currentTarget.parentElement.parentElement.style.display = 'none';
 }
+
+// display create category form
+createNewCategoryBtns.forEach(btn => {
+    btn.addEventListener('click', ()=> {
+        newCategoryFormPopup.style.display = 'grid';
+    });
+    btn.addEventListener('click', hideParent);
+});
+
+//close create category form
+closeCategoryForm.addEventListener('click', ()=> {
+newCategoryFormPopup.style.display = 'none';
+});
+
+//delete category
+deleteCategoryBtns.forEach(btn=> {
+    btn.addEventListener('click', () => {
+        const selectedList = lists.find(list=> list.id === selectedListId);
+        /* Old code that didn't work:
+        let categoryArr = selectedList.categories;
+        categoryArr = categoryArr.filter(category=> !category.selected);
+        because console.log(categoryArr) 
+        and console.log(selectedList.categories)
+        yield different results 
+        and selectedList.categories was plugged into the later saveAndRenderselectedList()*/
+
+        // New code that work:
+        selectedList.categories = selectedList.categories.filter(category=> !category.selected);
+        selectedCategoryId = 'null'; 
+        saveAndRenderselectedList();
+    });
+    btn.addEventListener('click', hideParent);
+});
+
 
 // add new list object to lists array 
 //then save to localStorage and render UI
@@ -123,7 +221,8 @@ newListForm.addEventListener('submit', e => {
     const list = createList(listName);
     newListInput.value = null;
     lists.push(list);
-    saveAndRender();
+    selectedCategoryId = 'null';
+    saveAndRenderAll();
 });    
 
 newListForm2.addEventListener('submit', e => {
@@ -133,35 +232,50 @@ newListForm2.addEventListener('submit', e => {
     const list = createList(listName);
     newListInput2.value = null;
     lists.push(list);
-    saveAndRender();
+    selectedCategoryId= 'null';
+    saveAndRenderAll();
 });  
 
-// add new task object in the tasks array 
-//within the selectedList object in the lists array
-//and save the new data to localStrage and render UI
-newTaskForm.addEventListener('submit', e => {
+// add new category to list
+newCategoryForm.addEventListener('submit', e => {
     e.preventDefault();
-    const taskName = newTaskInput.value;
-    if (taskName == null || taskName == '') return;
-    const task = createTask(taskName);
-    newTaskInput.value = null;
+    const categoryName = newCategoryInput.value;
+    if (categoryName == null || categoryName == '') return;        
+    const category = createCategory(categoryName);
+    newCategoryInput.value = null;
     const selectedList = lists.find(list => list.id === selectedListId);
-    selectedList.tasks.push(task);
-    saveAndRender();
-});   
+    selectedCategoryId = category.id
+    selectedList.categories.push(category);   
+    selectedList.categories.forEach(category => {
+        if (category.id !== selectedCategoryId) {
+            category.selected = false;
+        }
+    })
+    saveAndRenderselectedList();
+    newCategoryFormPopup.style.display = 'none';
+})
 
 // create list object
-function createList(name) {
-    return {id: Date.now().toString(), name: name, tasks: []}; //tasks are items
+function createList(listName) {
+    return {id: Date.now().toString(), name: listName, categories: []}; 
 }
 
-function createTask(name) {
-    return {id: Date.now().toString(), name: name, complete: false}
+function createCategory(categoryName) {
+    return {id: Date.now().toString(), name: categoryName, tasks: [], selected: true};//tasks are items
 }
 
-function saveAndRender() {
+function createTask(taskName, categoryName) {
+    return {id: Date.now().toString(), name: taskName, category: categoryName, complete: false};
+}
+
+function saveAndRenderAll() {
     save();
-    render();
+    renderAll();
+}
+
+function saveAndRenderselectedList(){
+    save();
+    renderSelectedList();
 }
 
 //saving the stringify current lists array and 
@@ -169,45 +283,101 @@ function saveAndRender() {
 function save() {
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
     localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_CATEGORY_ID_KEY, selectedCategoryId);
 }
 
 // render all DOM elements
-function render() {    
+function renderAll() {    
     clearElement(listsContainer);  
     clearElement(listsContainer2); 
     renderList();
 
+    renderSelectedList();
+}
+
+function renderSelectedList() {
     const selectedList = lists.find(list => list.id === selectedListId);
     if(selectedListId == 'null') {
         listDisplayContainer.style.display = 'none';
     } else {
         listDisplayContainer.style.display = '';
-        listTitleElement.innerText = selectedList.name;
+        listTitleElement.innerText = selectedList.name; 
+        clearElement(categoriesContainer);
+        renderCategories(selectedList);      
+        const tasksContainer = document.querySelectorAll('.category .items');
         renderTaskCount(selectedList);
-        clearElement(tasksContainer);
-        renderTasks(selectedList);
+        tasksContainer.forEach(taskContainer=> clearElement(taskContainer));
+        renderTasks(selectedList); 
+    }      
+}
+
+function renderCategories(selectedList) {
+    selectedList.categories.forEach(category => {
+        const categoryElement = document.importNode(categoryTemplate.content, true);
+        const radioBtn = categoryElement.querySelector('input[type="radio"]');
+        const categoryLabel = categoryElement.querySelector('label');
+        radioBtn.setAttribute('name', 'category');
+        radioBtn.id = category.id;
+        radioBtn.checked = category.selected;
+        categoryLabel.htmlFor = category.id;
+        const tasksContainer = categoryElement.querySelector('ul.items');
+        tasksContainer.id = category.name;
+        categoryLabel.append(category.name);
+        categoriesContainer.appendChild(categoryElement);
+    })
+
+        if (selectedCategoryId !== 'null') {
+            const categoryTitleRadioBtns = document.querySelectorAll('input[type="radio"]');
+            categoryTitleRadioBtns.forEach(categoryTitleRadioBtn=> { 
+                if(categoryTitleRadioBtn.id === selectedCategoryId) {
+                    categoryTitleRadioBtn.parentElement.parentElement.classList.add('selected-category');
+                }
+            }) 
+            
+            // render newItemForm within selectedCategory
+            const selectedCategoryWrapper = document.querySelector('.selected-category');
+            const newItemFormElement = document.importNode(newItemFormTemplate.content, true);
+            selectedCategoryWrapper.appendChild(newItemFormElement);            
+        }  
+} 
+// create DOM element for each task objects in the selectedList 
+// and append elements to tasksContainer
+//if I coould add a class for each li.item that match the id of the ul.items it belong
+//I can append them to the 
+function renderTasks(selectedList) {
+    const categoriesArr = selectedList.categories;
+    for (let i=0; i<categoriesArr.length; i++) {
+        const tasksArr = categoriesArr[i].tasks;
+        tasksArr.forEach(task => {
+            const taskElement = document.importNode(taskTemplate.content, true);
+            const checkbox = taskElement.querySelector('input');
+            checkbox.id = task.id;
+            checkbox.checked = task.complete;
+            const label = taskElement.querySelector('label');
+            label.htmlFor = task.id;
+            label.append(task.name); 
+            const tasksContainers = document.querySelectorAll('.category ul.items');
+            for (let j= 0; j<tasksContainers.length; j++) {
+                if (task.category === tasksContainers[j].id) {
+                    tasksContainers[j].appendChild(taskElement);
+                }
+            }
+        })
     }
 }
 
-// create DOM element for each task objects in the selectedList 
-// and append elements to tasksContainer
-function renderTasks(selectedList) {
-    selectedList.tasks.forEach(task => {
-        const taskElement = document.importNode(taskTemplate.content, true);
-        const checkbox = taskElement.querySelector('input');
-        checkbox.id = task.id;
-        checkbox.checked = task.complete;
-        const label = taskElement.querySelector('label');
-        label.htmlFor = task.id;
-        label.append(task.name);
-        tasksContainer.appendChild(taskElement);
-    })
-}
-
+// create a new array from all the tasks array, then filter out the ones that are imcompleted
+//then calculate its length. The number will be the imcomplete task count.
 function renderTaskCount(selectedList) {
-    const incompleteTaskCount = selectedList.tasks.filter(task => !task.complete).length;
-    const taskString = incompleteTaskCount === 1 || incompleteTaskCount === 0 ? 'item' : 'items';
-    listCountElement.innerHTML =`<span>${incompleteTaskCount}</span>  ${taskString} left`;
+    const categoriesArr = selectedList.categories;
+        let totalTasks = [];
+        for (let i = 0; i<categoriesArr.length; i++){
+                totalTasks = totalTasks.concat(categoriesArr[i].tasks);
+        }
+        const incompleteTaskCount = totalTasks.filter(task => !task.complete).length;
+        const taskString = incompleteTaskCount === 1 || incompleteTaskCount === 0 ? 'item' : 'items';
+        listCountElement.innerHTML =`<span>${incompleteTaskCount}</span>  ${taskString} left`;             
+
 }
 // create DOM element for each list objects in the lists array
 // and append elements to listsContainer
@@ -237,4 +407,5 @@ function clearElement(element) {
     }
 }
 
-render()
+renderAll()
+
